f3b513531b30b28762f297941e53a6e9ab212575
riska amanda <riskaamanda6778@gmail.com>
2026-01-17 11:52:14 +0700

    Menambahkan Profile

---
+++ pages/profile.php
@@ -0,0 +1,9 @@
+<div class="card shadow-sm">
+    <div class="card-body">
+        <h4>Profile</h4>
+        <hr>
+        <p><b>Nama:</b> Admin</p>
+        <p><b>Email:</b> admin@gmail.com</p>
+        <p><b>Role:</b> Administrator</p>
+    </div>
+</div>
